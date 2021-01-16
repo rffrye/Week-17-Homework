01b3a53ad395f3c19c1610c590045845aca834e4
RFFRYE <rffrye@users.noreply.github.com>
2021-01-16 18:02:58 -0500

    created routes and models folder.

---
+++ routes/api.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const Workout = require('../models/workout');
+
+router.post('/api/workouts', (req, res) => {
+	Workout.create({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workouts', (req, res) => {
+	Workout.find({})
+		.limit(7)
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workouts/range', ({ query }, res) => {
+	Workout.find({ day: { $gte: query.start, $lte: query.end } })
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+	Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } }, { new: true, runValidators: true })
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,8 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useFindAndModify: false
 });
 
-app.use(require('./routes/api.js'));
-app.use(require('./routes/view.js'));
+app.use(require('./routes/api'));
+app.use(require('./routes/views'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
